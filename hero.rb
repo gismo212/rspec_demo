5c01f0507a04be0d3fd5dadf22e5f5ac67b39720
Elegard <whiteandy21321@gmail.com>
2023-07-29 19:06:38 +0300

    add hero entity

--- hero.rb
+++ hero.rb
@@ -1,4 +1,30 @@
-require '/hero'
-
 class Hero
+  def initialize(name, health=100)
+    @name = name.downcase 
+    @health = health
+  end 
+
+  def name
+    @name 
+  end
+
+
+  def power_up
+    @health += 10
+  end
+
+  def power_down
+    @health -= 10
+  end
+
+  def hero_info
+    "#{name}#{@health}"
+  end
 end
+
+
+hero = Hero.new 'SKWA'
+puts hero.name
+puts hero.power_up
+puts hero.hero_info
+puts hero.power_down
