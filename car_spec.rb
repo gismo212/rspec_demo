e7f5bafc1c3e0d2a95eef851aed97273052a267f
Elegard <whiteandy21321@gmail.com>
2023-08-03 17:27:57 +0300

    add test to car

---
+++ car_spec.rb
@@ -0,0 +1,20 @@
+require './car'
+
+describe Car do
+  it "must return range" do
+
+    #arrange
+
+    car = Car.new
+
+    #act
+
+    car.add_fuel
+
+    #assert
+
+    expect(car.range).to eq 200
+
+  end
+
+end
